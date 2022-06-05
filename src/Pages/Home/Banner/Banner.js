d297e1a535dc99f704892f50a278d6f40f1e9fd9
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-06 02:01:32 +0600

    front added in button

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,3 +1,5 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import bannerImg from "../../../img/banner.png";
 
@@ -7,20 +9,21 @@ const Banner = () => {
       <div className="md:px-20 px-5">
         <div class="hero min-h-screen">
           <div class="hero-content flex-col lg:flex-row-reverse">
-            <img src={bannerImg} />
+            <img className="order-2 md:order-1" src={bannerImg} />
             <div className="order-1 md:order-2">
               <h1 class="text-3xl font-bold">
                 IT Management Service & <br /> Support for Your Business.
               </h1>
 
-              <p class="py-6 pr-28">
+              <p class="py-6 md:pr-28">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut
                 assumenda excepturi exercitationem quasi. In deleniti eaque aut
                 repudiandae et a id nisi.
               </p>
 
-              <button class="btn btn-primary  rounded-full text-white">
-                Explore More
+              <button class="btn btn-primary  rounded-full text-white mb-10 hover:bg-transparent hover:text-secondary duration-300">
+                Explore More{" "}
+                <FontAwesomeIcon className="ml-1" icon={faArrowRight} />
               </button>
             </div>
           </div>
