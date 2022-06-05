c5da4af7ced34577c0486c02de7ee731ebf51524
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-06 01:32:58 +0600

    added banner asection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home/Home/Home";
 import Navbar from "./Shared/Navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
