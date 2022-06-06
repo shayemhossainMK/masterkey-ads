780ce841e5a52d805408a803b079eabd2a2db3dc
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-06 09:20:05 +0600

    done

--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -30,85 +30,87 @@ export default class Reviews extends Component {
       slidesToScroll: 1,
     };
     return (
-      <div className="px-5 md:px-20 py-14">
-        <div className="text-center mb-10">
-          <p className="font-bold text-secondary text-3xl">Reviews</p>
-        </div>
-        <Slider ref={(c) => (this.slider = c)} {...settings}>
-          <div key={1} className=" mx-10 pr-5">
-            <div className="md:flex">
-              <img className="max-w-sm" src={slider1} alt="" />
-              <div className="md:px-10">
-                <h4 className="text-xl font-bold my-3">Samuel Howells</h4>
-                <p className="md:pr-48">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Rerum autem aspernatur impedit velit at deserunt laborum,
-                  cupiditate reiciendis nisi repellat. Lorem ipsum dolor sit
-                  amet, consectetur adipisicing elit. Qui, dolor!
-                </p>
+      <div className="bg-gray-100">
+        <div className="px-5 md:px-20 py-14">
+          <div className="text-center mb-10">
+            <p className="font-bold text-secondary text-3xl">Reviews</p>
+          </div>
+          <Slider ref={(c) => (this.slider = c)} {...settings}>
+            <div key={1} className=" mx-10 pr-5">
+              <div className="md:flex">
+                <img className="max-w-sm" src={slider1} alt="" />
+                <div className="md:px-10">
+                  <h4 className="text-xl font-bold my-3">Samuel Howells</h4>
+                  <p className="md:pr-48">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Rerum autem aspernatur impedit velit at deserunt laborum,
+                    cupiditate reiciendis nisi repellat. Lorem ipsum dolor sit
+                    amet, consectetur adipisicing elit. Qui, dolor!
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div key={2} className=" md:mx-10 pr-5">
-            <div className="md:flex">
-              <img className="max-w-sm" src={slider5} alt="" />
-              <div className="md:px-10">
-                <h4 className="my-3 font-bold text-xl">William Blake</h4>
-                <p className=" md:pr-48">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Rerum autem aspernatur impedit velit at deserunt laborum,
-                  cupiditate reiciendis nisi repellat. Lorem ipsum dolor sit
-                  amet consectetur adipisicing elit.
-                </p>
+            <div key={2} className=" md:mx-10 pr-5">
+              <div className="md:flex">
+                <img className="max-w-sm" src={slider5} alt="" />
+                <div className="md:px-10">
+                  <h4 className="my-3 font-bold text-xl">William Blake</h4>
+                  <p className=" md:pr-48">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Rerum autem aspernatur impedit velit at deserunt laborum,
+                    cupiditate reiciendis nisi repellat. Lorem ipsum dolor sit
+                    amet consectetur adipisicing elit.
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div key={3} className=" mx-10 pr-5">
-            <div className="md:flex">
-              <img className="max-w-sm" src={slider2} alt="" />
-              <div className=" md:px-10">
-                <h4 className="text-xl font-bold my-3">Hayden Hutchinson</h4>
-                <p className=" md:pr-48">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Rerum autem aspernatur impedit velit at deserunt laborum,
-                  cupiditate reiciendis nisi repellat. Lorem ipsum dolor sit
-                  amet consectetur adipisicing elit. Pariatur aspernatur
-                  quibusdam, adipisci architecto maiores fugiat assumenda odit
-                  et aliquam aliquid?
-                </p>
+            <div key={3} className=" mx-10 pr-5">
+              <div className="md:flex">
+                <img className="max-w-sm" src={slider2} alt="" />
+                <div className=" md:px-10">
+                  <h4 className="text-xl font-bold my-3">Hayden Hutchinson</h4>
+                  <p className=" md:pr-48">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Rerum autem aspernatur impedit velit at deserunt laborum,
+                    cupiditate reiciendis nisi repellat. Lorem ipsum dolor sit
+                    amet consectetur adipisicing elit. Pariatur aspernatur
+                    quibusdam, adipisci architecto maiores fugiat assumenda odit
+                    et aliquam aliquid?
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div key={4} className=" mx-10 pr-5">
-            <div className="md:flex">
-              <img className="max-w-sm" src={slider3} alt="" />
-              <div className="md:mx-10">
-                <h4 className="font-bold text-xl my-3">Laura Waters</h4>
-                <p className="md:pr-48">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Rerum autem aspernatur impedit velit at deserunt laborum,
-                  cupiditate reiciendis nisi repellat. Lorem ipsum dolor sit
-                  amet consectetur adipisicing elit. Accusamus, quis
-                  consequuntur! Veritatis itaque hic ducimus expedita.
-                  Exercitationem sequi repellat hic!
-                </p>
+            <div key={4} className=" mx-10 pr-5">
+              <div className="md:flex">
+                <img className="max-w-sm" src={slider3} alt="" />
+                <div className="md:mx-10">
+                  <h4 className="font-bold text-xl my-3">Laura Waters</h4>
+                  <p className="md:pr-48">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Rerum autem aspernatur impedit velit at deserunt laborum,
+                    cupiditate reiciendis nisi repellat. Lorem ipsum dolor sit
+                    amet consectetur adipisicing elit. Accusamus, quis
+                    consequuntur! Veritatis itaque hic ducimus expedita.
+                    Exercitationem sequi repellat hic!
+                  </p>
+                </div>
               </div>
             </div>
+          </Slider>
+          <div className="mt-10" style={{ textAlign: "center" }}>
+            <button
+              className=" px-6 mr-3 py-2 rounded-full shadow-md"
+              onClick={this.previous}
+            >
+              <FontAwesomeIcon icon={faAngleLeft} />
+            </button>
+            <button
+              className=" px-6 py-2 rounded-full shadow-md"
+              onClick={this.next}
+            >
+              <FontAwesomeIcon icon={faAngleRight} />
+            </button>
           </div>
-        </Slider>
-        <div className="mt-10" style={{ textAlign: "center" }}>
-          <button
-            className=" px-6 mr-3 py-2 rounded-full shadow-md"
-            onClick={this.previous}
-          >
-            <FontAwesomeIcon icon={faAngleLeft} />
-          </button>
-          <button
-            className=" px-6 py-2 rounded-full shadow-md"
-            onClick={this.next}
-          >
-            <FontAwesomeIcon icon={faAngleRight} />
-          </button>
         </div>
       </div>
     );
--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -7,86 +7,88 @@ import linkedin from "../../img/social-media/linkedin-icon.png";
 
 const Footer = () => {
   return (
-    <div className="py-10 ">
-      <div className="grid grid-cols-2 md:grid-cols-4">
-        <div className="md:mx-auto pl-2 md:pl-0">
-          <p className="text-secondary text-xl mb-3">We are social </p>
-          <p className="">FOLLOW US</p>
-          <br />
-          <div className="flex">
-            <a href="">
-              <img className="w-7" src={facebook} alt="" />
+    <div className="bg-gray-100">
+      <div className="py-10 shadow-2xl ">
+        <div className="grid grid-cols-2 md:grid-cols-4">
+          <div className="md:mx-auto pl-2 md:pl-0">
+            <p className="text-secondary text-xl mb-3">We are social </p>
+            <p className="">FOLLOW US</p>
+            <br />
+            <div className="flex">
+              <a href="">
+                <img className="w-7" src={facebook} alt="" />
+              </a>
+              <a href="">
+                <img className="w-7" src={instra} alt="" />
+              </a>
+              <a href="">
+                <img className="w-7" src={linkedin} alt="" />
+              </a>
+            </div>
+            <br />
+            <h5 className="text-2xl font-bold">LOGO</h5>
+          </div>
+          <div className="mx-auto">
+            <p className="text-secondary text-xl mb-3">Links</p>
+            <a className="font-bold" href="">
+              ADVERTISERS
             </a>
-            <a href="">
-              <img className="w-7" src={instra} alt="" />
+            <br />
+            <br />
+            <a className="font-bold" href="">
+              PUBLISHERS
             </a>
-            <a href="">
-              <img className="w-7" src={linkedin} alt="" />
+            <br />
+            <br />
+            <a className="font-bold" href="">
+              INFLUENCERS
+            </a>
+            <br />
+            <br />
+            <a className="font-bold mt-2" href="">
+              AD FORMATS
             </a>
           </div>
-          <br />
-          <h5 className="text-2xl font-bold">LOGO</h5>
-        </div>
-        <div className="mx-auto">
-          <p className="text-secondary text-xl mb-3">Links</p>
-          <a className="font-bold" href="">
-            ADVERTISERS
-          </a>
-          <br />
-          <br />
-          <a className="font-bold" href="">
-            PUBLISHERS
-          </a>
-          <br />
-          <br />
-          <a className="font-bold" href="">
-            INFLUENCERS
-          </a>
-          <br />
-          <br />
-          <a className="font-bold mt-2" href="">
-            AD FORMATS
-          </a>
-        </div>
-        <div className="mx-auto mt-10 md:mt-0">
-          <p className="text-secondary text-xl mb-3">Documentation</p>
-          <a className="font-bold" href="">
-            TERMS & CONDITIONS
-          </a>
-          <br />
-          <br />
-          <a className="font-bold" href="">
-            PRIVACY POLICY{" "}
-          </a>
-          <br />
-          <br />
-          <a className="font-bold" href="">
-            CANCELLATION POLICY
-          </a>
-          <br />
-          <br />
-          <a className="font-bold" href="">
-            BLOG
-          </a>
-        </div>
-        <div className="mx-auto mt-10 md:mt-0">
-          <p className="text-secondary text-xl mb-3">Support</p>
+          <div className="mx-auto mt-10 md:mt-0">
+            <p className="text-secondary text-xl mb-3">Documentation</p>
+            <a className="font-bold" href="">
+              TERMS & CONDITIONS
+            </a>
+            <br />
+            <br />
+            <a className="font-bold" href="">
+              PRIVACY POLICY{" "}
+            </a>
+            <br />
+            <br />
+            <a className="font-bold" href="">
+              CANCELLATION POLICY
+            </a>
+            <br />
+            <br />
+            <a className="font-bold" href="">
+              BLOG
+            </a>
+          </div>
+          <div className="mx-auto mt-10 md:mt-0">
+            <p className="text-secondary text-xl mb-3">Support</p>
 
-          <a className="mb-2 font-bold" href="">
-            FAQ
-          </a>
-          <br />
-          <br />
-          <a className="mb-2 font-bold" href="">
-            MEDIA KIT
-          </a>
-          <br />
-          <br />
-          <a className="font-bold" href="">
-            CONTACT US
-          </a>
-          <br />
-          <br />
+            <a className="mb-2 font-bold" href="">
+              FAQ
+            </a>
+            <br />
+            <br />
+            <a className="mb-2 font-bold" href="">
+              MEDIA KIT
+            </a>
+            <br />
+            <br />
+            <a className="font-bold" href="">
+              CONTACT US
+            </a>
+            <br />
+            <br />
+          </div>
         </div>
       </div>
     </div>
